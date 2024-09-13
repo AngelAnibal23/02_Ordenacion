507d7c85eb3e08238f7b717f2c351b4027bf0bed
WninaE <wninae@unjbg.edu.pe>
2024-09-13 14:49:05 -0500

    Agregando el menu para hacer los intercambios

--- InterDirecto.cpp
+++ InterDirecto.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 	int n; 
-
+	int opcion;
 	cout<<"Digite cuantos elementos tendra el arreglo: "; 
 	cin>>n; 
 
@@ -13,13 +13,58 @@ int main(){
 		cout<<"Digite el elemento ["<<i<<"]: "; 
 		cin>>array[i];  
 	} 
+	
 
 	cout<<"\nImprimiendo el arreglo: "; 
 	for(int i=0; i<n; i++){
 		cout<<array[i]<<" ";  
 	} 
+	cout<<endl; 
 	
+	do{
+ 
+		
+	cout<<"\nA continuacion se le muestran distitintos metodos de ordenacion :"<<endl; 
+	cout<<"(1) Intercambio Directo por la derecha. "<<endl; 
+	cout<<"(2) Intercambio directo por la izquierda. "<<endl; 
+	cout<<"(3) Intercambio directo -----------"<<endl; 
+	cout<<"(4) Intercambio directo ------------"<<endl;
+	cout<<"(0) Salir."<<endl;  
+	cout<<"Digite el metodo de ordenacion que desea realizar: "; 
+	cin>>opcion; 
+	cout<<endl; 
 	
+	switch(opcion){
+		case 1: {
+			
+			break;
+		}
+		case 2: {
+			
+			break;
+		}
+		case 3: {
+			
+			break;
+		}
+		
+		case 4: {
+			
+			break;
+		}
+		case 0:{
+			cout<<"Saliendo del programa...."; 
+			opcion = 0; 
+			break;
+		}
+		default:{
+			cout<<"Opcion invalida. "; 
+			break;
+		}
+	}
+	
+		
+	}while(opcion != 0); 
 	
 	return 0; 	
 }
